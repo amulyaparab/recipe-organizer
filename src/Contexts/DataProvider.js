4c2cb8bfc0469d21968b602c9a73fa85f55e48a2
Amulya Parab <amulyaparab101@gmail.com>
2023-07-01 22:03:09 +0530

    fix: fixed search bug

--- src/Contexts/DataProvider.js
+++ src/Contexts/DataProvider.js
@@ -23,7 +23,7 @@ export const DataProvider = ({ children }) => {
       case "SEARCH_BY":
         return {
           ...state,
-          filteredRecipes: recipes.filter((recipe) =>
+          filteredRecipes: state.filteredRecipes.filter((recipe) =>
             state.category === "ingredients"
               ? recipe[state.category]
                   .join("")
